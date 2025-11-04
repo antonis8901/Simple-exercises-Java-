88526fb8c4792a89bb18cecca95a7db067a9c166
Chamamtzis Antonis <antonisx89@hotmail.gr>
2025-11-05 01:27:32 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+    Human ppl = new Human ("Tony",15);
+    ppl.wakeUp();
+    ppl.sleep();
+    ppl.born();
+    ppl.die();
+    ppl.breathe();
+    }
+}
